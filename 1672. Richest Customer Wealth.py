af0f957a07a2a967e07aeea1b229b0e691ad1877
Shailesh L Prabhu <shaileshprabhu3@gmail.com>
2024-11-04 15:11:04 -0800

    Time: 4ms(89.31%) Space: 18.31mb(89.39%)

---
+++ 1672. Richest Customer Wealth.py
@@ -0,0 +1,11 @@
+accounts = [[1,2,3],[3,2,1]]
+
+wealth = []
+for account in accounts:
+    sum =0
+    for money in account:
+        sum+=money
+    wealth.append(sum)
+
+print(max(wealth))
+
---
+++ 977. Squares of a sorted Array.py
@@ -0,0 +1,5 @@
+nums = [-4,-1,0,3,10]
+
+nums = [i**2 for i in nums]
+
+print(sorted(nums))
