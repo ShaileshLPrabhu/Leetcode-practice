3f4bbfbdecd4ae762139755e3aa6aae0d7df9fc1
Shailesh L Prabhu <shaileshprabhu3@gmail.com>
2024-11-25 16:18:42 -0800

    Time: 0ms(100%) Space:16.6MB(26.16%)

---
+++ 2108.  Find First Palindromic String in Array.py
@@ -0,0 +1,8 @@
+words = ["abc","car","ada","racecar","cool"]
+
+for i in words:
+    if i == i[::-1]:
+        print(i)
+        break
+
+print("nu")
---
+++ 2798 - Numbr of Employees who met the target.py
@@ -0,0 +1,11 @@
+hours = [0,1,2,3,4]
+target = 2
+
+c=0
+for i in hours:
+    if i >=target:
+        c+=1
+
+print(c)
+
+
