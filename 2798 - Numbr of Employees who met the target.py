583dea8ba1a8ef57cde4bdf8afd8e9c99dd2e155
Shailesh L Prabhu <shaileshprabhu3@gmail.com>
2024-11-25 16:20:47 -0800

    Time: 1ms(19.27%) Space:16.55MB(71.74%)

--- 2798 - Numbr of Employees who met the target.py
+++ 2798 - Numbr of Employees who met the target.py
@@ -1,11 +1,7 @@
-hours = [0,1,2,3,4]
-target = 2
-
-c=0
-for i in hours:
-    if i >=target:
-        c+=1
-
-print(c)
-
-
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        c=0
+        for i in hours:
+            if i >=target:
+                c+=1
+        return c
